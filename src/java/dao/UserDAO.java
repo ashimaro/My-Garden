45d3d5c99fcec0964699c13a7fd4d9ea218acafb
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-03-24 23:14:38 +0800

    create method for login

--- src/java/dao/UserDAO.java
+++ src/java/dao/UserDAO.java
@@ -11,6 +11,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -33,16 +35,73 @@ public class UserDAO {
             myPS.setString(1, user.getUsername());
             myPS.setString(2, user.getEmail());
             myPS.setString(3, user.getPassword());
-           
+
             myPS.executeUpdate();
 
-       } catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
 
         } finally {
-           
+
             con.close();
         }
     }
 
+    public User retrieveUser(String username, String password) throws SQLException {
+        ResultSet rs = null;
+        User user = null;
+        try {
+            String mySQLQuery = "select * from tblaccount where Username=? and Password=?";
+            PreparedStatement ps = con.prepareStatement(mySQLQuery);
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                user = new User();
+
+                user.setEmail(username);
+                user.setPassword(password);
+                /**
+                 * user.setAutoIndex(rs.getInt("autoindex"));
+                 *
+                 * try { String myQ = "update users set logindatetime=? where
+                 * userId=" + rs.getInt("userid");
+                 *
+                 * PreparedStatement myPS = con.prepareStatement(myQ);
+                 *
+                 * DateTimeFormatter myFormatObj =
+                 * DateTimeFormatter.ofPattern("yyy-MM-dd HH:mm:ss");
+                 * LocalDateTime myDateObj = LocalDateTime.now(); String
+                 * formattedDate = myDateObj.format(myFormatObj);
+                 *
+                 * myPS.setString(1, formattedDate);
+                 *
+                 * myPS.executeUpdate();
+                 *
+                 * } catch (SQLException e) {
+                 * System.out.println(e.getMessage());
+                 *
+                 * } finally { try { con.close(); } catch (SQLException ex) {
+                 * Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE,
+                 * null, ex); } }
+                 */
+            }
+
+            con.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            con.close();
+        }
+
+        return user;
+    }
 }
