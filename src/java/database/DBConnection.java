37ea9d66694818bdd9f78cb260b927288aca1330
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-03-24 23:30:50 +0800

    add comment and review

--- src/java/database/DBConnection.java
+++ src/java/database/DBConnection.java
@@ -10,23 +10,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    
-    private static final String URL = "jdbc:mysql://localhost:3306/dbgarden";
-    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    //define constants
+    private static final String URL = "jdbc:mysql://localhost:3306/dbgarden";//connect to the database
+    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver"; 
     private static final String USERNAME = "root";
     private static final String PASSWORD = "admin";
     
+    //declare a static variable to hold the connection obj
     private static Connection conn;
     
+    //create static method to establish a connection to db and return to Connection object
     public static Connection getConnection() throws ClassNotFoundException{
         try {
-            Class.forName(DRIVER_NAME);
+            Class.forName(DRIVER_NAME); //load the driver class
             
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD); // create a connection obj
         } catch (ClassNotFoundException | SQLException e){
-            e.getMessage();
+            e.getMessage(); // handle any exceptions that occur
         }
-        return conn;
+        return conn;// return the connection obj
     }
     
 }
