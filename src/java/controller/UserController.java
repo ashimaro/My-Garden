687558ae96ef937d523c3dee683c32b03244c9ed
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-03-27 00:15:13 +0800

    add condition for password

--- src/java/controller/UserController.java
+++ src/java/controller/UserController.java
@@ -93,29 +93,41 @@ public class UserController extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
-    private void insertUser(HttpServletRequest req, HttpServletResponse resp)
-            throws SQLException, ServletException, IOException, ClassNotFoundException {
+  private void insertUser(HttpServletRequest req, HttpServletResponse resp)
+        throws SQLException, ServletException, IOException, ClassNotFoundException {
 
-        //get all data from register page
-        String username = req.getParameter("username");
-        String email = req.getParameter("email");
-        String password = req.getParameter("password");
+    //get all data from register page
+    String username = req.getParameter("username");
+    String email = req.getParameter("email");
+    String password = req.getParameter("password");
 
-        //keep data into javabeans
-        User newUser = new User();
+    //check if password meets criteria
+    boolean isValidPassword = password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$");
 
-        newUser.setUsername(username);
-        newUser.setEmail(email);
-        newUser.setPassword(password);
-
-        //pass the bean to DAO
-        UserDAO userdao = new UserDAO();
-        userdao.save(newUser);
-
-        //save the bean as attribute and pass to view
-        req.setAttribute("user", newUser);
-        resp.sendRedirect("index.jsp");
+    if (!isValidPassword) {
+        //password does not meet criteria, show error message
+        req.setAttribute("errorMessage", "Password must be at least 8 characters long and contain at least one special character and one capital letter.");
+        req.getRequestDispatcher("registerAccount.jsp").forward(req, resp);
+        return;
     }
+
+    //keep data into javabeans
+    User newUser = new User();
+
+    newUser.setUsername(username);
+    newUser.setEmail(email);
+    newUser.setPassword(password);
+
+    //pass the bean to DAO
+    UserDAO userdao = new UserDAO();
+    userdao.save(newUser);
+
+    //save the bean as attribute and pass to view
+    req.setAttribute("user", newUser);
+    req.setAttribute("successMessage", "User saved successfully!");
+    resp.sendRedirect("index.jsp");
+}
+
 
     private void searchUser(HttpServletRequest req, HttpServletResponse resp)
             throws SQLException, ServletException, IOException, ClassNotFoundException {
@@ -131,28 +143,10 @@ public class UserController extends HttpServlet {
                 User user = userDAO.retrieveUser(username, password);
 
                 if (user != null) {
-                    //usertype = req.getParameter("usertype");
                     HttpSession session = req.getSession();
-                    //session.setAttribute("userid", user.getUserId());
                     session.setAttribute("user", user);
                     resp.sendRedirect("mainpage.jsp");
-                    // session.setAttribute("loginusertype", user.getUserType());
 
-                    //loginusertype = user.getUserType();
-                    /* if (loginusertype.equals("ADMIN"))
-                    {
-                       
-                        //updatelogin(request, response);
-                        resp.sendRedirect("dashboardadmin.jsp");
-                    }
-                    else
-                    {
-                        resp.sendRedirect("dashboard.jsp");
-                    }
-                   
-                    
-                   //resp.sendRedirect("dashboard.jsp");
-                     */
                 } else {
                     req.setAttribute("error", "Invalid email or password!");
                     RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
